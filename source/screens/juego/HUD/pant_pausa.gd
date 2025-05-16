7a13fb5f33cd79a74af9ba448dcb4d447f15cc1b
Lalo <eduardo.jair.bs@gmail.com>
2025-05-15 19:39:52 -0600

    Pausa wii

--- source/screens/juego/HUD/pant_pausa.gd
+++ source/screens/juego/HUD/pant_pausa.gd
@@ -10,18 +10,18 @@
 class_name PantPausa extends CanvasLayer
 var pausaSFX = preload("res://resources/audio/pausa.mp3")
 var pausaExitSFX = preload("res://resources/audio/pausaExit.mp3")
+var pausaGuardar = preload("res://resources/audio/WiiClick.mp3")
 var on_pause:bool
 
 func _ready() -> void:
-	$fondo/hbox/btnContinuar.grab_focus()
-
-func _input(event: InputEvent) -> void:
-	pass
+	$contenedor/Continuar/btnContinuar.grab_focus()
 
 func _on_btn_continuar_pressed() -> void:
 	procesar()
 
 func _on_btn_guardar_pressed() -> void:
+	$SFX.stream = pausaGuardar
+	$SFX.play()
 	GLOBAL.guardarPartida()
 
 func _on_btn_menu_prin_pressed() -> void:
@@ -29,13 +29,19 @@ func _on_btn_menu_prin_pressed() -> void:
 	SCN_FADE_IN.cambia_escena("res://source/screens/menus/pant_principal.tscn")
 
 func procesar():
+	if !on_pause:
+		get_tree().paused=!get_tree().paused
+		$contenedor.visible = !$contenedor.visible
 	on_pause = !on_pause
-	get_tree().paused=!get_tree().paused
 	var sonido = pausaSFX if on_pause else pausaExitSFX
 	$SFX.stream = sonido
-	$fondo.visible = !$fondo.visible
-	if $fondo.visible:
-		$fondo/hbox/btnContinuar.grab_focus()
+	if on_pause:
+		$ANPPausa.play("Start")
+		$contenedor/Continuar/btnContinuar.grab_focus()
 		$SFX.play()
 	else:
+		$ANPPausa.play_backwards("Start")
 		$SFX.play()
+		await get_tree().create_timer(0.7).timeout
+		$contenedor.visible = !$contenedor.visible
+		get_tree().paused=!get_tree().paused
