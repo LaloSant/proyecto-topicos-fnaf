998f70b23440393a8994bca6bbe306125ce97095
yael8895 <yael.sampayo.01082005@gmail.com>
2025-05-19 08:05:31 -0600

    Cristal terminado
    Si

--- source/screens/juego/cristal/cristal.gd
+++ source/screens/juego/cristal/cristal.gd
@@ -10,11 +10,15 @@
 extends Node2D
 @onready var ruta_parte_1F= $Rutas/RtaParte1/RtaFParte1
 @onready var ruta_parte_2F = $Rutas/RtaParte2/RtaFParte2
+@onready var ruta_parte_3F = $Rutas/RtaParte3/RtaFParte3
+@onready var ruta_parte_4F = $Rutas/RtaParte4/RtaFParte4
+@onready var ruta_parte_5F = $Rutas/RtaParte5/RtaFParte5
+@onready var ruta_parte_6F = $Rutas/RtaParte6/RtaFParte6
 
 func _ready() -> void:
 	$Personaje.defaultSpeed = GLOBAL.pers_default_speed * 0.5
-	$Enemigos/Guardia1.speed*=2.5
-	$Enemigos/Guardia2.speed*=3
+	##$Enemigos/Guardia1.speed*=2.5
+	##$Enemigos/Guardia2.speed*=3
 	match GLOBAL.marker_actual:
 		
 			GLOBAL.MarkerPosicion.mk_CristalEntrada:
@@ -29,5 +33,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	$Enemigos/Guardia1.actualizarPos(ruta_parte_1F,delta)
 	$Enemigos/Guardia2.actualizarPos(ruta_parte_2F,delta)
+	$Enemigos/Guardia3.actualizarPos(ruta_parte_3F,delta)
+	$Enemigos/Guardia4.actualizarPos(ruta_parte_4F,delta)
+	$Enemigos/Guardia5.actualizarPos(ruta_parte_5F,delta)
+	$Enemigos/Guardia6.actualizarPos(ruta_parte_6F,delta)
 
 	
